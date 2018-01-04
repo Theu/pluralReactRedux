605457b09c1be760c51d38dcf0bd38ed2e037b7c
Matteo Contini <matteo.contini@zalando-lounge.de>
2018-01-04 15:26:12 +0100

    general setup

---
+++ src/index.js
@@ -0,0 +1,2 @@
+// entry point of the javascript
+console.log('I am the entry point of the js AKA index.js');
---
+++ src/index.spec.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('First test just to show something', () => {
+    it('should pass', () => {
+        expect(true).toEqual(true);
+    });
+});
